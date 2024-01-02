1eef04b2869863d179ee1ff72f0bdf14b75c2493
Erin Wise <erinwise88@gmail.com>
2024-01-02 13:32:45 -0500

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,33 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      
+      nodes_to_visit = nodes_to_visit + node['children']
+    return None
+
+  def bread_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node['value'])
+      nodes_to_visit = nodes_to_visit + node['children']
+    
+    return result 
+'''
+Method: takes a node as input and return a list containing the values of the root and all its child nodes
+Initialize an empty output list
+Initialize a list of nodes to visit and add the root node to it
+While are nodes in the nodes to visit list:
+  Remove the first node from the nodes to visit list
+  Add its value to the output list
+  Add its children (if any) to the end of the nodes to visit list
+Return the output list
+'''
